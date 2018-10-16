a0b156dc5268b3975ea850674ffb053181e25447
arrecis59 <arrecis59@gmail.com>
2018-10-15 23:19:38 -0600

    arrecis59
    created

---
+++ MUESTREOAUDITORIA/auditoria.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace MUESTREOAUDITORIA
+{
+    partial class auditoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(auditoria));
+            this.navegador1 = new CapaDiseno.Navegador();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Gpb_Data = new System.Windows.Forms.GroupBox();
+            this.Txt_cant_Fisic = new System.Windows.Forms.TextBox();
+            this.Txt_cant_logic = new System.Windows.Forms.TextBox();
+            this.Txt_nombre = new System.Windows.Forms.TextBox();
+            this.Txt_codigo_producto = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Dgv_productos = new System.Windows.Forms.DataGridView();
+            this.cod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidadL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidadF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Gpb_Data.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_productos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // navegador1
+            // 
+            this.navegador1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.navegador1.Location = new System.Drawing.Point(11, 64);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(1213, 74);
+            this.navegador1.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(154)))), ((int)(((byte)(152)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1233, 42);
+            this.panel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "5302_MUESTREO Y AUDITORIA";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(42, 42);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1102, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 41);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1161, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Gpb_Data
+            // 
+            this.Gpb_Data.Controls.Add(this.Txt_cant_Fisic);
+            this.Gpb_Data.Controls.Add(this.Txt_cant_logic);
+            this.Gpb_Data.Controls.Add(this.Txt_nombre);
+            this.Gpb_Data.Controls.Add(this.Txt_codigo_producto);
+            this.Gpb_Data.Controls.Add(this.label5);
+            this.Gpb_Data.Controls.Add(this.label4);
+            this.Gpb_Data.Controls.Add(this.label3);
+            this.Gpb_Data.Controls.Add(this.label2);
+            this.Gpb_Data.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gpb_Data.Location = new System.Drawing.Point(46, 148);
+            this.Gpb_Data.Name = "Gpb_Data";
+            this.Gpb_Data.Size = new System.Drawing.Size(1138, 289);
+            this.Gpb_Data.TabIndex = 10;
+            this.Gpb_Data.TabStop = false;
+            // 
+            // Txt_cant_Fisic
+            // 
+            this.Txt_cant_Fisic.Location = new System.Drawing.Point(470, 235);
+            this.Txt_cant_Fisic.Multiline = true;
+            this.Txt_cant_Fisic.Name = "Txt_cant_Fisic";
+            this.Txt_cant_Fisic.Size = new System.Drawing.Size(199, 32);
+            this.Txt_cant_Fisic.TabIndex = 7;
+            // 
+            // Txt_cant_logic
+            // 
+            this.Txt_cant_logic.Location = new System.Drawing.Point(470, 172);
+            this.Txt_cant_logic.Multiline = true;
+            this.Txt_cant_logic.Name = "Txt_cant_logic";
+            this.Txt_cant_logic.Size = new System.Drawing.Size(199, 32);
+            this.Txt_cant_logic.TabIndex = 6;
+            // 
+            // Txt_nombre
+            // 
+            this.Txt_nombre.Location = new System.Drawing.Point(470, 105);
+            this.Txt_nombre.Multiline = true;
+            this.Txt_nombre.Name = "Txt_nombre";
+            this.Txt_nombre.Size = new System.Drawing.Size(435, 32);
+            this.Txt_nombre.TabIndex = 5;
+            // 
+            // Txt_codigo_producto
+            // 
+            this.Txt_codigo_producto.Location = new System.Drawing.Point(470, 38);
+            this.Txt_codigo_producto.Multiline = true;
+            this.Txt_codigo_producto.Name = "Txt_codigo_producto";
+            this.Txt_codigo_producto.Size = new System.Drawing.Size(277, 32);
+            this.Txt_codigo_producto.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(297, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Cantidad Fisica";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(297, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Cantidad Lógica";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(297, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(297, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Código de producto";
+            // 
+            // Dgv_productos
+            // 
+            this.Dgv_productos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Dgv_productos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_productos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cod,
+            this.nombre,
+            this.cantidadL,
+            this.cantidadF});
+            this.Dgv_productos.Location = new System.Drawing.Point(14, 455);
+            this.Dgv_productos.Name = "Dgv_productos";
+            this.Dgv_productos.RowTemplate.Height = 24;
+            this.Dgv_productos.Size = new System.Drawing.Size(1207, 304);
+            this.Dgv_productos.TabIndex = 9;
+            // 
+            // cod
+            // 
+            this.cod.HeaderText = "CodProducto";
+            this.cod.Name = "cod";
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // cantidadL
+            // 
+            this.cantidadL.HeaderText = "Cantidad Logica";
+            this.cantidadL.Name = "cantidadL";
+            // 
+            // cantidadF
+            // 
+            this.cantidadF.HeaderText = "Cantidad Fisica";
+            this.cantidadF.Name = "cantidadF";
+            // 
+            // muestreo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(168)))), ((int)(((byte)(167)))));
+            this.ClientSize = new System.Drawing.Size(1231, 768);
+            this.Controls.Add(this.Gpb_Data);
+            this.Controls.Add(this.Dgv_productos);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "muestreo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "muestreo";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Gpb_Data.ResumeLayout(false);
+            this.Gpb_Data.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_productos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CapaDiseno.Navegador navegador1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox Gpb_Data;
+        private System.Windows.Forms.TextBox Txt_cant_Fisic;
+        private System.Windows.Forms.TextBox Txt_cant_logic;
+        private System.Windows.Forms.TextBox Txt_nombre;
+        private System.Windows.Forms.TextBox Txt_codigo_producto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView Dgv_productos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidadL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidadF;
+    }
+}
